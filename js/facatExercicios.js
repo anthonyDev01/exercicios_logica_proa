6b5a461b4512ea906e3ca3e9565ca8e8d39562a9
ANTHONY <anthonychukwudiDev@outlook.com>
2024-04-14 20:47:54 -0300

    subindo o restante dos exercicios da FACCAT

--- js/facatExercicios.js
+++ js/facatExercicios.js
@@ -77,26 +77,6 @@ const exerciciosFacat = [
     },
 
     {
-        nome: "dias de vida",
-        exercicio: function ex8() {
-            var idade = parseInt(prompt("Digite sua idade: "));
-            var mesversario = parseInt(
-                prompt(
-                    "Digite quantos meses fazem desde sua ultimo aniversario: "
-                )
-            );
-            let dias = parseInt(
-                prompt(
-                    "Digite quantos dias se passaram em relação ao mês que você nasceu: "
-                )
-            );
-            let dias_de_vida = idade * 365 + mesversario * 30 + dias;
-
-            alert(`voce tem ${dias_de_vida} dias de vida`);
-        },
-    },
-
-    {
         nome: "votos",
         exercicio: function ex9() {
             let numero_total_votos = parseInt(
@@ -206,24 +186,6 @@ const exerciciosFacat = [
     {
         nome: "positivo ou negativo",
         exercicio: function ex16() {
-            let numero = parseInt(prompt("Digite um número: "));
-            if (numero >= 0) alert("O número é Positivo");
-            else alert("O número é Negativo!");
-        },
-    },
-
-    {
-        nome: "positivo ou negativo",
-        exercicio: function ex17() {
-            let numero = parseInt(prompt("Digite um número: "));
-            if (numero >= 0) alert("O número é Positivo");
-            else alert("O número é Negativo!");
-        },
-    },
-
-    {
-        nome: "positivo ou negativo",
-        exercicio: function ex17() {
             let numero = parseInt(prompt("Digite um número: "));
             if (numero >= 0) alert("O número é Positivo");
             else alert("O número é Negativo!");
@@ -421,28 +383,6 @@ const exerciciosFacat = [
     },
 
     {
-        nome: "estoque",
-        exercicio: function ex28() {
-            let quantProdAtual = parseInt(
-                prompt("Digite a quantidade de produtos em estoque: ")
-            );
-            let quantMax = parseInt(
-                prompt("Digite a quantidade máxima que o estoque comporta: ")
-            );
-            let quantMin = parseInt(
-                prompt("Digite a quantidade mínima que o estoque precisa: ")
-            );
-
-            let quantMedia = (quantMax + quantMin) / 2;
-            let efetuar = "";
-
-            if (quantProdAtual >= quantMedia) efetuar = "Não efetuar compra";
-            else efetuar = "Efetuar compra";
-            alert(`A quantidade media de estoque é: ${quantMedia}\n${efetuar}`);
-        },
-    },
-
-    {
         nome: "positivo, negativo ou zero",
         exercicio: function ex29() {
             let numero = parseInt(prompt("Digite um número: "));
@@ -526,6 +466,382 @@ const exerciciosFacat = [
             )
                 alert(`Os valores não forma um triângulo.`);
             else alert(`Os valores formam um triângulo.`);
+        },
+    },
+
+    {
+        nome: "Verificar vencedor",
+        exercicio: function ex34() {
+            alert(`Você escolheu o ${addTextoBtn[31]}! Clique para continuar.`);
+
+            let time1 = prompt("Digite o nome do primeiro time: ");
+            let time2 = prompt("Digite o nome do segundo time: ");
+
+            let gols1 = parseInt(
+                prompt("Digite o número de gols do primeiro time: ")
+            );
+            let gols2 = parseInt(
+                prompt("Digite o número de gols do segundo time: ")
+            );
+
+            let vencedor = "";
+
+            if (gols1 > gols2) vencedor = time1;
+            else if (gols1 < gols2) vencedor = time2;
+            else vencedor = "EMPATE";
+
+            alert("O vencedor é: " + vencedor);
+        },
+    },
+    {
+        nome: "Comparar números",
+        exercicio: function ex35() {
+            alert(`Você escolheu o ${addTextoBtn[32]}! Clique para continuar.`);
+
+            let numero1 = parseInt(prompt("Digite um número: "));
+            let numero2 = parseInt(prompt("Digite outro número: "));
+
+            let comparacao =
+                numero1 > numero2
+                    ? "Primeiro maior"
+                    : numero1 == numero2
+                    ? "Números iguais"
+                    : "Segundo maior";
+
+            alert(`Sobre os números: ${comparacao}`);
+        },
+    },
+    {
+        nome: "Calcular preço do combustível",
+        exercicio: function ex36() {
+            alert(`Você escolheu o ${addTextoBtn[33]}! Clique para continuar.`);
+
+            alert(
+                `Bem vindo ao posto de gasolina da Faccat\nÁlcool: R$2.90 / Gasolina: R$3.30`
+            );
+            alert(
+                `Álcool até 20L: 3% de desconto / Acima disso: 5% de desconto\nGasolina até 20L: 4% de desconto / Acima disso: 6% de desconto`
+            );
+
+            let combustivel = prompt(
+                "Escolha o tipo de combustível (A - álcool, G - gasolina): "
+            );
+            let litros = parseInt(
+                prompt("Digite quantos litros deseja abastecer: ")
+            );
+            let preco = 0;
+
+            if (litros > 20) {
+                if (combustivel.toUpperCase() === "A")
+                    preco = litros * 2.9 * (1 - 0.05);
+                else if (combustivel.toUpperCase() === "G")
+                    preco = litros * 3.3 * (1 - 0.05);
+            } else {
+                if (combustivel.toUpperCase() === "A")
+                    preco = litros * 2.9 * (1 - 0.03);
+                else if (combustivel.toUpperCase() === "G")
+                    preco = litros * 3.3 * (1 - 0.03);
+            }
+
+            alert(`O valor a ser pago é de R$${preco.toFixed(2)}`);
+        },
+    },
+    {
+        nome: "Calcular soma e produto de idades",
+        exercicio: function ex37() {
+            alert(`Você escolheu o ${addTextoBtn[34]}! Clique para continuar.`);
+
+            let idadeHomem1 = parseInt(
+                prompt("Digite a idade do primeiro homem: ")
+            );
+            let idadeHomem2 = parseInt(
+                prompt("Digite a idade do segundo homem: ")
+            );
+            let idadeMulher1 = parseInt(
+                prompt("Digite a idade da primeira mulher: ")
+            );
+            let idadeMulher2 = parseInt(
+                prompt("Digite a idade da segunda mulher: ")
+            );
+
+            let maiorH, menorH, maiorM, menorM;
+
+            if (idadeHomem1 > idadeHomem2) {
+                maiorH = idadeHomem1;
+                menorH = idadeHomem2;
+            } else {
+                maiorH = idadeHomem2;
+                menorH = idadeHomem1;
+            }
+
+            if (idadeMulher1 > idadeMulher2) {
+                maiorM = idadeMulher1;
+                menorM = idadeMulher2;
+            } else {
+                maiorM = idadeMulher2;
+            }
+
+            let somaIdades = maiorH + menorM;
+            let multIdades = menorH * maiorM;
+
+            alert(
+                `A soma das idades do homem mais velho com a mulher mais nova é: ${somaIdades}`
+            );
+            alert(
+                `O produto das idades do homem mais novo com a mulher mais velha é: ${multIdades}`
+            );
+        },
+    },
+    {
+        nome: "Calcular preço das frutas",
+        exercicio: function ex38() {
+            alert(`Você escolheu o ${addTextoBtn[35]}! Clique para continuar.`);
+
+            alert(
+                `A maçã custa R$1.80 até 5kg, e R$1.50 acima disso\nE morangos custam R$2.50 até 5kg, e R$2.20 acima disso.\nTemos também 10% de desconto se sua compra ultrapassar 8kg ou R$25.`
+            );
+
+            let macas = parseInt(
+                prompt("Digite quantos Kg de maçã irá levar: ")
+            );
+            let morangos = parseInt(
+                prompt("Ditire quantos Kg de morango irá levar: ")
+            );
+
+            let compra = 0;
+            let kgFinal = morangos + macas;
+
+            let totalMorangos = morangos > 5 ? morangos * 2.2 : morangos * 2.5;
+
+            let totalMacas = macas > 5 ? macas * 1.5 : macas * 1.8;
+
+            compra = totalMacas + totalMorangos;
+            if (kgFinal > 8 || compra > 25) compra -= compra * 0.1;
+
+            alert(`Preço final: ${compra}`);
+        },
+    },
+    {
+        nome: "Verificar usuário e senha",
+        exercicio: function ex39() {
+            alert(`Você escolheu o ${addTextoBtn[36]}! Clique para continuar.`);
+
+            let login, senha, tentaLogin, tentaSenha;
+
+            login = 1234;
+            senha = 9999;
+
+            tentaLogin = parseInt(prompt("Digite o Login: "));
+
+            if (tentaLogin !== login) {
+                alert("Login inválido!");
+            } else {
+                tentaSenha = parseInt(prompt("Digite a Senha: "));
+
+                if (tentaSenha !== senha) alert("Senha inválida!");
+                else alert("Acesso Permitido!");
+            }
+        },
+    },
+    {
+        nome: "Calcular preço do produto com desconto",
+        exercicio: function ex40() {
+            alert(`Você escolheu o ${addTextoBtn[37]}! Clique para continuar.`);
+
+            let nomeProd, quantidade, precoUni, total, desconto;
+
+            nomeProd = prompt("Digite o nome do produto: ");
+            precoUni = parseFloat(
+                prompt("Digite o preço unitário do produto: ")
+            );
+            quantidade = parseInt(prompt("Digite a quantidade adquirida: "));
+
+            total = quantidade * precoUni;
+
+            if (quantidade > 10) desconto = total * 0.05;
+            else {
+                if (quantidade > 5 && quantidade <= 10) desconto = total * 0.03;
+                else desconto = total * 0.02;
+            }
+
+            alert(
+                `Produto: ${nomeProd}\nQuantidade: ${quantidade}\nPreço unitário: ${precoUni}\nTotal a pagar: ${
+                    total - desconto
+                }`
+            );
+        },
+    },
+    {
+        nome: "Calcular aproveitamento do aluno",
+        exercicio: function ex41() {
+            alert(`Você escolheu o ${addTextoBtn[38]}! Clique para continuar.`);
+
+            let nota1, nota2, nota3, mediaExercicios, mediaFinal, notaMedia;
+
+            nota1 = parseFloat(prompt("Digite a Primeira Nota: "));
+            nota2 = parseFloat(prompt("Digite a Segunda Nota: "));
+            nota3 = parseFloat(prompt("Digite a Terceira Nota: "));
+            mediaExercicios = parseFloat(
+                prompt("Digite a Média dos Exercícios: ")
+            );
+
+            mediaFinal = (nota1 + nota2 * 2 + nota3 * 3 + mediaExercicios) / 7;
+
+            if (mediaFinal >= 9.0) notaMedia = "A";
+            else if (mediaFinal >= 7.5 && mediaFinal < 9.0) notaMedia = "B";
+            else if (mediaFinal >= 6.0 && mediaFinal < 7.5) notaMedia = "C";
+            else notaMedia = "D";
+
+            alert("O Aproveitamento Final é: " + notaMedia);
+        },
+    },
+    {
+        nome: "Verificar aposentadoria do empregado",
+        exercicio: function ex42() {
+            alert(`Você escolheu o ${addTextoBtn[39]}! Clique para continuar.`);
+
+            let vaiAposentar = "";
+            let anoAtual = Date().getFullYear();
+
+            let codFuncionario = parseInt(
+                prompt("Digite seu ID de funcionário: ")
+            );
+            let nascimento = parseInt(prompt("Digite seu ano de nascimento: "));
+            let ingressoEmpresa = parseInt(
+                prompt("Digite seu ano de entrada na empresa: ")
+            );
+
+            let idadeFuncionario = anoAtual - nascimento;
+            let tempoTrabalhado = anoAtual - ingressoEmpresa;
+
+            if (
+                idadeFuncionario >= 65 ||
+                tempoTrabalhado >= 30 ||
+                (idadeFuncionario >= 60 && tempoTrabalhado >= 25)
+            )
+                vaiAposentar = "Requerer aposentadoria";
+            else vaiAposentar = "Não requerer";
+
+            alert(
+                `Funcionário: ${codFuncionario}\nIdade: ${idadeFuncionario}\nTempo na Empresa: ${tempoTrabalhado}\nApto para aposentar: ${vaiAposentar}`
+            );
+        },
+    },
+    {
+        nome: "Ler valores e fazer divisão",
+        exercicio: function ex43() {
+            alert(`Você escolheu o ${addTextoBtn[40]}! Clique para continuar.`);
+
+            let num1, num2;
+
+            num1 = parseInt(prompt("Digite o Primeiro valor: "));
+
+            do {
+                alert("Aviso: o segundo valor não pode ser 0...");
+                num2 = parseInt(prompt("Digite o Segundo valor: "));
+            } while (num2 === 0);
+
+            alert(`${num1} ${num2}`);
+        },
+    },
+    {
+        nome: "Ler valores e fazer divisão (ENQUANTO)",
+        exercicio: function ex44() {
+            alert(`Você escolheu o ${addTextoBtn[41]}! Clique para continuar.`);
+
+            let num1,
+                num2 = 0;
+            num1 = parseInt(prompt("Digite o Primeiro valor: "));
+
+            while (num2 === 0) {
+                alert("Aviso: o segundo valor não pode ser 0...");
+                num2 = parseInt(prompt("Digite o Segundo valor: "));
+            }
+
+            alert(`${num1} ${num2}`);
+        },
+    },
+    {
+        nome: "Ler valores e fazer divisão (DO-WHILE)",
+        exercicio: function ex45() {
+            alert(`Você escolheu o ${addTextoBtn[42]}! Clique para continuar.`);
+
+            let num1, num2;
+
+            num1 = parseInt(prompt("Digite o Primeiro valor: "));
+
+            do {
+                num2 = parseInt(prompt("Digite o Segundo valor: "));
+                if (num2 === 0) alert("VALOR INVÁLIDO");
+            } while (num2 === 0);
+
+            alert(`${num1} ${num2}`);
+        },
+    },
+    {
+        nome: "Ler valores e fazer divisão com mensagem de erro",
+        exercicio: function ex46() {
+            alert(`Você escolheu o ${addTextoBtn[43]}! Clique para continuar.`);
+
+            let num1,
+                num2 = 0;
+
+            num1 = parseInt(prompt("Digite o Primeiro valor: "));
+            while (num2 === 0) {
+                num2 = parseInt(prompt("Digite o Segundo valor: "));
+                if (num2 === 0) alert("VALOR INVÁLIDO");
+            }
+
+            alert(`${num1} ${num2}`);
+        },
+    },
+    {
+        nome: "Calcular média de notas",
+        exercicio: function ex47() {
+            alert(`Você escolheu o ${addTextoBtn[44]}! Clique para continuar.`);
+
+            let nota1, nota2, media;
+            nota1 = -1;
+
+            while (nota1 < 0 || nota1 > 10) {
+                nota1 = parseInt(prompt("Digite a nota da Avaliação 1: "));
+                if (nota1 === 0) alert("VALOR INVÁLIDO");
+            }
+
+            do {
+                nota2 = parseInt(prompt("Digite a nota da Avaliação 2: "));
+                if (nota2 === 0) alert("VALOR INVÁLIDO");
+            } while (nota2 < 0 || nota2 > 10);
+
+            media = (nota1 + nota2) / 2;
+
+            alert(`Média do aluno: ${media}`);
+        },
+    },
+    {
+        nome: "Calcular média de notas com opção de novo cálculo",
+        exercicio: function ex48() {
+            alert(`Você escolheu o ${addTextoBtn[45]}! Clique para continuar.`);
+
+            let nota1, nota2, media, novoCalc;
+
+            do {
+                nota1 = -1;
+
+                while (nota1 < 0 || nota1 > 10) {
+                    nota1 = parseInt(prompt("Digite a nota da Avaliação 1: "));
+                    if (nota1 === 0) alert("VALOR INVÁLIDO");
+                }
+
+                do {
+                    nota2 = parseInt(prompt("Digite a nota da Avaliação 2: "));
+                    if (nota2 === 0) alert("VALOR INVÁLIDO");
+                } while (nota2 < 0 || nota2 > 10);
+
+                media = (nota1 + nota2) / 2;
+                alert(`Média do aluno: ${media}`);
+                novoCalc = prompt("Novo Cálculo? (S/N)").toUpperCase();
+            } while (novoCalc === "S");
         },
     },
 ];
